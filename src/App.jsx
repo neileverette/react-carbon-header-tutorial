466e7f9d235a8b9fa6f1c22a94d3ba667c6188a1
NEIL EVERETTE <neileverette@mac.lan>
2025-03-19 14:20:06 -0500

    Added Automation Hub JSX page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,6 @@
-/**
- * @license
- *
- * Copyright IBM Corp. 2025
- *
- * This source code is licensed under the Apache-2.0 license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 
 import React, { useState } from 'react';
+import AutomationHub from './AutomationHub';
 import {
   SIDE_NAV_TYPE,
   SideNav,
@@ -366,5 +358,6 @@ function App() {
     </Theme>
   );
 }
+
 
 export default App;
